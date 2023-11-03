1da187a79eb1139bd76ea0545bbb4c87fb06e8c4
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-11-03 23:08:08 +0000

    fix

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $.post("http://0.0.0.0:5001/api/v1/places_search/", function(data){
-    	$.each(data, function(index, place) {
+    	$.each(data, function(index, places) {
 	let placeInfo = '<article>';
-	placeInfo += $('h2').text(place.name);
-	placeInfo += $('.price_by_night').text(place.price_by_night);
-	placeInfo += $('.max_guest').text(place.max_guest);
-	placeInfo += $('.number_rooms').text(place.number_rooms);
-	placeInfo += $('.number_bathrooms').text(place.number_bathrooms);
-	placeInfo += $('.description').text(place.description);
+	placeInfo += $('h2').text(places.name);
+	placeInfo += $('.price_by_night').text(places.price_by_night);
+	placeInfo += $('.max_guest').text(places.max_guest);
+	placeInfo += $('.number_rooms').text(places.number_rooms);
+	placeInfo += $('.number_bathrooms').text(places.number_bathrooms);
+	placeInfo += $('.description').text(places.description);
 	$('.places').append(placeInfo);
 	});
     });
