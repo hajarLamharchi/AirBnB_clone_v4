e159af65efb53506d215a624ef7580af4429c8b4
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-11-04 19:12:47 +0000

    some fixing

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
     $.post("http://0.0.0.0:5001/api/v1/places_search/", function(data){
     	$.each(data, function(index, places) {
-	let placeInfo = '<article>';
-	placeInfo += $('h2').text(places.name);
-	placeInfo += $('.price_by_night').text(places.price_by_night);
-	placeInfo += $('.max_guest').text(places.max_guest);
-	placeInfo += $('.number_rooms').text(places.number_rooms);
-	placeInfo += $('.number_bathrooms').text(places.number_bathrooms);
-	placeInfo += $('.description').text(places.description);
-	$('.places').append(placeInfo);
+			let placeInfo = '<article>';
+            placeInfo += '<h2>' + place.name + '</h2>';
+            placeInfo += '<div class="price_by_night">$' + place.price_by_night + '</div>';
+            placeInfo += '<div class="max_guest">' + place.max_guest + ' Guests</div>';
+            placeInfo += '<div class="number_rooms">' + place.number_rooms + ' Rooms</div>';
+            placeInfo += '<div class="number_bathrooms">' + place.number_bathrooms + ' Bathrooms</div>';
+            placeInfo += '<div class="description">' + place.description + '</div>';
+            placeInfo += '</article>';
+            $('.places').append(placeInfo);
 	});
     });
 });
